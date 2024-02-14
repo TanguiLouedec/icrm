75004aed466c42a4b02fb28a766fe4ed68e7fd79
Koffingering <tangui.louedec@gmail.com>
2024-02-14 15:15:17 +0100

    FEAT: added db findAll for ressources

--- src/app/ressource/page.tsx
+++ src/app/ressource/page.tsx
@@ -1,7 +1,15 @@
+import { findAllRessource } from "@/db/queries/ressource"
 export default async function Ressource() {
+  const ressources = await findAllRessource()
   return (
     <div>
-
+      {ressources.map(ressource => {
+        return (
+          <div key={ressource.id}>
+            {ressource.tittle}
+          </div>
+        )
+      })}
     </div>
   ) 
 }

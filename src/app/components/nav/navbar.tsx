37afbb4f1c049b08ba5a2e5979686d00b8b9ec0f
Koffingering <tangui.louedec@gmail.com>
2024-02-14 02:57:34 +0100

    FEAT: added sass support and basic navbar styling

--- src/app/components/nav/navbar.tsx
+++ src/app/components/nav/navbar.tsx
@@ -1,8 +1,9 @@
+import styles from "./navbar.module.scss"
 import Link from "next/link"
 
 export default function Navbar() {
   return (
-    <div>
+    <div className={styles.container}>
       <Link href="/">Home</Link>
       <Link href="/ressource">Ressources</Link>
     </div>
